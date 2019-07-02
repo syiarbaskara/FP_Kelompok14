34435d630b6c5e246aa24c91ed77b4bd49b68b6a
syiarbaskara <46886335+syiarbaskara@users.noreply.github.com>
2019-07-02 18:38:16 +0700

    Add files via upload

--- JS/phone-list.component.js
+++ JS/phone-list.component.js
@@ -10,11 +10,30 @@ component('phoneList', {
   '<a href="video.html"><button class="submenu" id="video"><font face="Helvetica"><b>Video</b></font></button></a>'+
   '<a href="games.html"><button class="submenu" id="games"><font face="Helvetica"><b>Games</b></font></button></a>'+
   '<a href="about_us.html"><button class="submenu" id="about_us"><font face="Helvetica"><b>About Us</b></font></button></a>'+
-  '<div class="search-container" align="right">'+
+  '<a href="TulisArtikel.html"><img src=IMG/write.png width="30px" height="30px" style=";margin-top:14px;cursor:pointer;margin-left:19%;"></a>'+
+  '<div class="search-container">'+
   '<form action="/action_page.php">'+
   '<input type="text" placeholder="Search.." name="search">'+
   '<button type="submit"><i class="fa fa-search"></i></button>'+
   '</form>'+
+  '</div>'+
+  '<div id="mySidenav" class="sidenav">'+
+  '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>'+
+  '<center><img src="IMG/a8.jpg" width="70px" height="70px;">'+
+  '<hr>'+
+  '<h3  style="color:white;"><a style="color:white; margin-right:40px;" href="isi_akun.html">Indah</a></h3>'+
+  '<p style="color:white;"><i class="fa fa-user-circle-o"></i>Akun</p>'+
+  '<p style="color:white;"><i class="fas fa-sign-out-alt"></i>Log Out</p>'+
+  '</center>'+
+  '</div>'+
+  '<div class="rigthnav" onclick="openNav()">'+
+  '<p></p>'+
+  '<p></p>'+
+  '<p></p>'+
+  '<p></p>'+
+  '<div class="bar1"></div>'+
+  '<div class="bar2"></div>'+
+  '<div class="bar3"></div>'+
   '</div>'+
   '</div>'+
   '</header>'
